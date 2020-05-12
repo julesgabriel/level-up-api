ef87b701c372e05df6745d4fc9a26f9793ebf908
Jules <julesgabriel>
2020-05-12 16:26:51 +0200

    create token

---
+++ configuration/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "facebook_api_key": "512005503038693",
+    "facebook_api_secret": "0472a7b4231583eefac669d27a04215c",
+    "callback_url": "http://localhost:3000/auth/facebook/callback",
+}
